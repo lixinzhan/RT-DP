6dd1fd62b244fe48afad8472537122491ebbe11f
Lixin Zhan <lixinzhan@gmail.com>
2024-11-28 16:29:26 -0500

    formated date time for email.

--- bin/email_result.sh
+++ bin/email_result.sh
@@ -7,7 +7,10 @@ echo
 echo INFO_TIME: `date '+%Y-%m-%d %H:%M:%S'`  Sending email to confirm data retrieved ...
 echo
 
-echo ">>> $(hostname)  ${DATESTAMP}-${TIMESTAMP} <<<" > ${RTDRTMP}/email.txt
+BKDATE=`echo $DATESTAMP | awk '{print substr($0,1,4)"-"substr($0,5,2)"-"substr($0,7,2)}'`
+BKTIME=`echo $TIMESTAMP | awk '{print substr($0,1,2)":"substr($0,3,2)":"substr($0,5,2)}'`
+
+echo ">>> $(hostname)  ${BKDATE} ${BKTIME} <<<" > ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
 echo "Patients data retrieved and archived to files below:" >> ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
@@ -22,7 +25,7 @@ echo >> ${RTDRTMP}/email.txt
 cat ${LOGFILE} | grep INFO_TIME | sed 's/INFO_TIME: //g' >> ${RTDRTMP}/email.txt
 
 
-cat ${RTDRTMP}/email.txt | mail -s "RT-DP running result from $(hostname) at ${TIMESTAMP} on ${DATESTAMP}" \
+cat ${RTDRTMP}/email.txt | mail -s "RT-DP running result from $(hostname) at ${BKTIME} on ${BKDATE}" \
 	-aFrom:"${FROMMAIL}" \
 	-aReply-to:"${ADMINMAIL}" \
 	${ADMINMAIL} ${OTHERMAIL}
