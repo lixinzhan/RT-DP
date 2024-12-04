f37736f6aab19ff818d3cd19047aa64d2423eb33
Lixin Zhan <lixinzhan@gmail.com>
2024-12-04 14:32:53 -0500

    added in zipkeepdays

--- bin/finalize.sh
+++ bin/finalize.sh
@@ -7,9 +7,12 @@ rm -rf ${RTDRDATA}/__DATETIMESTAMP__
 #rm -rf ${RTDRTMP}/*.zip
 
 #
-# Keep only the last 15 patient data archive files
+# Keep only the patient data archive files for the last ${ZIPKEEPDAYX} days
 #
 mv ${RTDRARXIV}/README.md ${RTDRTMP}/README.arx.backup
-cd ${RTDRARXIV} && ls -1tr | head -n -91 | xargs --no-run-if-empty rm -rf
+#cd ${RTDRARXIV} && ls -1tr | head -n -91 | xargs --no-run-if-empty rm -rf
+if compgen -G "${RTDRARXIV}/${ZIPCOMMON}*.zip" > /dev/null 2>&1; then
+    find ${RTDRARXIV} -mtime +${ZIPKEEPDAYS} -delete
+fi
 mv ${RTDRTMP}/README.arx.backup ${RTDRARXIV}/README.md
 
