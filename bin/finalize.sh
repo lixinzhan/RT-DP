123aef87dbd033f327667863bdac1a134b6242c7
Lixin Zhan <lixinzhan@gmail.com>
2024-12-04 13:51:54 -0500

    updated to keep 15 days files

--- bin/finalize.sh
+++ bin/finalize.sh
@@ -7,9 +7,9 @@ rm -rf ${RTDRDATA}/__DATETIMESTAMP__
 #rm -rf ${RTDRTMP}/*.zip
 
 #
-# Keep only the last 7 patient data archive files
+# Keep only the last 15 patient data archive files
 #
 mv ${RTDRARXIV}/README.md ${RTDRTMP}/README.arx.backup
-cd ${RTDRARXIV} && ls -1tr | head -n -21 | xargs --no-run-if-empty rm -rf
+cd ${RTDRARXIV} && ls -1tr | head -n -91 | xargs --no-run-if-empty rm -rf
 mv ${RTDRTMP}/README.arx.backup ${RTDRARXIV}/README.md
 
