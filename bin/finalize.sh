12e153b2643884ef126546c2e09918d7adfc6e72
Lixin Zhan <lixinzhan@gmail.com>
2024-12-04 14:34:30 -0500

    fixed a typo

--- bin/finalize.sh
+++ bin/finalize.sh
@@ -7,7 +7,7 @@ rm -rf ${RTDRDATA}/__DATETIMESTAMP__
 #rm -rf ${RTDRTMP}/*.zip
 
 #
-# Keep only the patient data archive files for the last ${ZIPKEEPDAYX} days
+# Keep only the patient data archive files for the last ${ZIPKEEPDAYS} days
 #
 mv ${RTDRARXIV}/README.md ${RTDRTMP}/README.arx.backup
 #cd ${RTDRARXIV} && ls -1tr | head -n -91 | xargs --no-run-if-empty rm -rf
