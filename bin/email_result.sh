2912a13d205baf04723157b1cbc8b07ee3539054
Lixin Zhan <lixinzhan@gmail.com>
2024-12-08 15:46:27 -0500

    updated email text to show disk space usage.

--- bin/email_result.sh
+++ bin/email_result.sh
@@ -12,20 +12,31 @@ BKTIME=`echo $TIMESTAMP | awk '{print substr($0,1,2)":"substr($0,3,2)}'`
 
 echo ">>>>>> $(hostname)  ${BKDATE} ${BKTIME} <<<<<<" > ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
+
 echo "Patients data retrieved and archived to files below:" >> ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
 cd ${RTDRARXIV} && ls -lh | awk '{print $5, "\t", $9}' | grep $(basename ${ZIPCOMMON}) >> ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
+
 echo "** If no file listed or file sizes are not reasonable, please check your RT-DP system !!!" >> ${RTDRTMP}/email.txt
 echo "** Files can be accessed on ${EHLP_SERVER%%.*} in folder ${EHLP_DATA} for routine tests." >> ${RTDRTMP}/email.txt
 echo "** Files are available on $(hostname) in folder ${RTDRARXIV} for offline treatment!" >> ${RTDRTMP}/email.txt
 echo "** Offline Tx Steps: ${DOCROOT}${EMTXDOC}" >> ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
+
 echo "------------------------------------" >> ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
 echo "Time information for key steps: " >> ${RTDRTMP}/email.txt
 echo >> ${RTDRTMP}/email.txt
 cat ${LOGFILE} | grep INFO_TIME | sed 's/INFO_TIME: //g' >> ${RTDRTMP}/email.txt
+echo >> ${RTDRTMP}/email.txt
+
+echo "------------------------------------" >> ${RTDRTMP}/email.txt
+echo >> ${RTDRTMP}/email.txt
+echo "Disk space info on which RT-DP is on: " >> ${RTDRTMP}/email.txt
+echo >> ${RTDRTMP}/email.txt
+echo $(df -h ${RTDRPATH}) >> ${RTDRTMP}/email.txt
+echo >> ${RTDRTMP}/email.txt
 
 
 cat ${RTDRTMP}/email.txt | mail -s "RT-DP running result from $(hostname) at ${BKTIME} on ${BKDATE}" \
