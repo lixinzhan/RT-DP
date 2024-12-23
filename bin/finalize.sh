8efedc2c8d13ec19abdc3e4f2c1da40679ccdd5b
Lixin Zhan <lixinzhan@gmail.com>
2024-12-23 10:23:28 -0500

    fixed path issue in compgen; and change find switch -mtime to -mmin with 6 hours added to avoid patial deleting issue for files of the same day.

--- bin/finalize.sh
+++ bin/finalize.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 
 source ./env.common
@@ -12,7 +12,9 @@ rm -rf ${RTDRDATA}/__DATETIMESTAMP__
 cp ${RTDRARXIV}/README.md ${RTDRARXIV}/README.bk
 rm ${RTDRARXIV}/README.md && mv ${RTDRARXIV}/README.bk ${RTDRARXIV}/README.md
 #cd ${RTDRARXIV} && ls -1tr | head -n -91 | xargs --no-run-if-empty rm -rf
-if compgen -G "${RTDRARXIV}/${ZIPCOMMON}*.zip" > /dev/null 2>&1; then
-    find ${RTDRARXIV} -mtime +${ZIPKEEPDAYS} -delete
+if compgen -G "${ZIPCOMMON}*.zip"; then
+    # find ${RTDRARXIV} -mtime +${ZIPKEEPDAYS} -delete
+    ## add 6 more hours to avoid deleting only some of the files from one day.
+    find ${RTDRARXIV} -mmin +$((${ZIPKEEPDAYS}*24*60+6*60)) -delete 
 fi
 
