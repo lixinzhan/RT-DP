c9675a0c014cb24861734c7c16e325d69a8573ea
Lixin Zhan <lixinzhan@gmail.com>
2024-12-24 08:13:25 -0500

    using zip file size to deltermine is a backup is reasonable, hence to delete a previous backup or not.

--- bin/finalize.sh
+++ bin/finalize.sh
@@ -1,20 +1,27 @@
-#!/bin/bash -x
+#!/bin/bash
 #
 
 source ./env.common
 
 rm -rf ${RTDRDATA}/__DATETIMESTAMP__
-#rm -rf ${RTDRTMP}/*.zip
+
 
 #
 # Keep only the patient data archive files for the last ${ZIPKEEPDAYS} days
 #
+
+# Refresh timestamp for README.md to avoid deletion.
 cp ${RTDRARXIV}/README.md ${RTDRARXIV}/README.bk
 rm ${RTDRARXIV}/README.md && mv ${RTDRARXIV}/README.bk ${RTDRARXIV}/README.md
-#cd ${RTDRARXIV} && ls -1tr | head -n -91 | xargs --no-run-if-empty rm -rf
-if compgen -G "${ZIPCOMMON}*.zip"; then
-    # find ${RTDRARXIV} -mtime +${ZIPKEEPDAYS} -delete
-    ## add 6 more hours to avoid deleting only some of the files from one day.
+
+# Estimate the minimal zip file size and find the actual zip file size
+NUMPT=`cat ${SQLOUTPATH}/_patient_list.uniq | wc -l`
+ZIPSMIN=$((${NUMPT}*${PLAN_PCT}/100*${PLANSIZE}*${ZIP2_PCT}/100))
+ZIPSIZE=`du -cm ${ZIPCOMMON}*.zip | grep total | awk '{print $1}'`
+
+# If zip file size is reasonalble, delete the backup ${ZIPKEEPDAYS} days ago.
+if [ "${ZIPSIZE}" -gt "${ZIPSMIN}" ]; then
+    # add 6 more hours to avoid deleting only some of the files from one day.
     find ${RTDRARXIV} -mmin +$((${ZIPKEEPDAYS}*24*60+6*60)) -delete 
 fi
 
