b238d16ab5e348d67428f8feb0f50bc93c022f93
Lixin Zhan <lixinzhan@gmail.com>
2025-11-20 16:36:27 -0500

    added 06_finalize.sh to remind the cleanup steps.

---
+++ bin/base_setup_scripts/06_finalize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+echo "1. disable ssh access by runnning commands below:"
+echo "  sudo ufw status numbered"
+echo "  sudo ufw delete <the-number>"
+echo "  sudo ufw reload"
+echo ""
+echo "2. remove residue public keys, gpg files etc"
+echo ""
+echo "3. disconnect iDrac from network"
+
